15d7c1ee762fd0fd3bce20dd2529732c21cfd929
FangFeiyue <18766131540@163.com>
2021-11-03 10:15:28 +0800

    菜单列表的实现

--- routes/menus.js
+++ routes/menus.js
@@ -4,6 +4,41 @@ const Menu = require('../models/menuSchema');
 
 router.prefix('/menu');
 
+router.get('/list', async (ctx) => {
+	try {
+		const { menuName, menuState } = ctx.request.query;
+		const params = {};
+
+		if (menuName) params.menuName = menuName;
+		if (menuState) params.menuState = menuState;
+
+		const list = (await Menu.find(params)) || [];
+
+		ctx.body = util.success(getTreeMenu(list));
+	} catch (err) {
+		ctx.body = util.fail(err.stack);
+	}
+});
+
+function getTreeMenu(list) {
+	return list.filter((item1) => {
+		item1._doc.children = [];
+		list.forEach((item2) => {
+			const parentIds = item2.parentId;
+			if (String(parentIds && parentIds[parentIds.length - 1]) === item1.id) {
+				item1._doc.children.push(item2);
+			}
+		});
+
+		if (item1._doc.children[0] && item1._doc.children[0].menuType == 2) {
+			// 快速判断按钮和菜单，用于后期做菜单按钮权限控制
+			item1._doc.action = item1._doc.children;
+		}
+
+		return item1.parentId[0] === null;
+	});
+}
+
 router.post('/operate', async (ctx) => {
 	const { action, _id, ...params } = ctx.request.body;
 	const info = {
@@ -23,8 +58,7 @@ router.post('/operate', async (ctx) => {
 			res = await Menu.findByIdAndRemove(_id);
 			res = await Menu.deleteMany({ parentId: { $all: [ _id ] } });
 		}
-
-		if (!res || res.deletedCount === 0) {
+		if (!res) {
 			ctx.body = util.fail('操作失败');
 		} else {
 			ctx.body = util.success(res, info[action] || '操作成功');
--- utils/utils.js
+++ utils/utils.js
@@ -1,34 +1,49 @@
 const CODE = {
-  SUCCESS: 200, // 成功
-  PARAM_ERROR: 10001, // 参数错误
-  USER_ACCOUNT_ERROR: 20001, // 账号或密码错误
-  USER_LOGIN_ERROR: 30001, // 用户未登录
-  BUSINESS_ERROR: 40001, // 业务请求失败
-  AUTH_ERROR: 50001 // 认证失败或 token 过期
+	SUCCESS: 200, // 成功
+	PARAM_ERROR: 10001, // 参数错误
+	USER_ACCOUNT_ERROR: 20001, // 账号或密码错误
+	USER_LOGIN_ERROR: 30001, // 用户未登录
+	BUSINESS_ERROR: 40001, // 业务请求失败
+	AUTH_ERROR: 50001 // 认证失败或 token 过期
 };
 
 module.exports = {
-  parge({ pageNum=1, pageSize=10 }) {
-    pageNum *= 1;
-    pageSize *= 1;
-    const skipIndex = (pageNum - 1) * pageSize;
-    return {
-      page: {
-        pageNum,
-        pageSize
-      },
-      skipIndex
-    };
-  },
-  success(data, msg, code = CODE.SUCCESS) {
-    return {
-      code, data, msg
-    };
-  },
-  fail(msg='', code = CODE.BUSINESS_ERROR) {
-    return {
-      msg, code
-    }
-  },
-  CODE
+	parge({ pageNum = 1, pageSize = 10 }) {
+		pageNum *= 1;
+		pageSize *= 1;
+		const skipIndex = (pageNum - 1) * pageSize;
+		return {
+			page: {
+				pageNum,
+				pageSize
+			},
+			skipIndex
+		};
+	},
+	success(data, msg, code = CODE.SUCCESS) {
+		return {
+			code,
+			data,
+			msg
+		};
+	},
+	fail(msg = '', code = CODE.BUSINESS_ERROR) {
+		return {
+			msg,
+			code
+		};
+	},
+	getTreeMenu(list) {
+		return list.filter((item1) => {
+			item1._doc.children = [];
+			list.forEach((item2) => {
+				const parentIds = item2.parentId;
+				if (String(parentIds && parentIds[parentIds.length - 1]) === item1.id) {
+					item1._doc.children.push(item2);
+				}
+			});
+			return item1.parentId[0] === null;
+		});
+	},
+	CODE
 };

1c62323c7f70ca14d969d8b6fdaa6a4d3a8f971a
FangFeiyue <18766131540@163.com>
2021-12-13 09:57:41 +0800

    休假申请

--- routes/leave.js
+++ routes/leave.js
@@ -1,8 +1,10 @@
 const router = require('koa-router')();
 const Utils = require('../utils/utils');
-const LeaveSchema = require('../models/leavSchema');
+const Leave = require('../models/leavSchema');
+const Dept = require('../models/deptSchema');
 const utils = require('../utils/utils');
 const { configure } = require('log4js');
+const leavSchema = require('../models/leavSchema');
 
 router.prefix('/leave');
 
@@ -18,9 +20,9 @@ router.get('/list', async (ctx) => {
 
 		if (applyState) params.applyState = applyState;
 
-		const query = LeaveSchema.find(params);
+		const query = Leave.find(params);
 		const list = await query.skip(skipIndex).limit(page.pageSize);
-		const total = await LeaveSchema.countDocuments(params);
+		const total = await Leave.countDocuments(params);
 		ctx.body = utils.success({
 			page: {
 				...page,
@@ -28,6 +30,55 @@ router.get('/list', async (ctx) => {
 			},
 			list
 		});
+	} catch (err) {
+		ctx.body = utils.fail(err.stack);
+	}
+});
+
+router.post('/operate', async (ctx) => {
+	try {
+		const { _id, action, ...params } = ctx.request.body;
+		const { authorization } = ctx.request.headers;
+		const { data } = utils.decode(authorization);
+
+		console.log(data);
+
+		if (action == 'create') {
+			let orderNo = 'XJ';
+			orderNo += utils.formateDate(new Date(), 'yyMMdd');
+			const count = await Leave.countDocuments();
+			params.orderNo = orderNo + count;
+
+			const id = data.deptId.pop();
+			const dept = await Dept.findById(id);
+			console.log('dept', dept);
+			const userList = await Dept.find({ deptName: { $in: [ '人事部门', '财务部门' ] } });
+			let auditUsers = dept.userName;
+			const auditFlows = [ { userId: dept.userId, userName: dept.userName, userEmail: dept.userEmail } ];
+			userList.forEach((item) => {
+				auditFlows.push({
+					userId: item.userId,
+					userName: item.userName,
+					userEmail: item.userEmail
+				});
+				auditUsers += ',' + item.userName;
+			});
+
+			params.auditUsers = auditUsers;
+			params.curAuditUserName = dept.userName;
+			params.auditFlows = auditFlows;
+			params.auditLogs = [];
+			params.applyUser = {
+				userId: data.userId,
+				userName: data.userName,
+				userEmail: data.userEmail
+			};
+			await Leave.create(params);
+			ctx.body = utils.success('', '创建成功');
+		} else {
+			await Leave.findByIdAndUpdate(_id, { applyState: 5 });
+			ctx.body = utils.success('', '操作成功');
+		}
 	} catch (err) {
 		ctx.body = utils.fail(err.stack);
 	}
--- utils/utils.js
+++ utils/utils.js
@@ -54,5 +54,24 @@ module.exports = {
 		}
 		return '';
 	},
+	formateDate(date, rule) {
+		date = new Date(date);
+		let fmt = rule || 'yyyy-MM-dd hh:mm:ss';
+		const o = {
+			'y+': date.getFullYear(),
+			'M+': date.getMonth(),
+			'd+': date.getDate(),
+			'h+': date.getHours(),
+			'm+': date.getMinutes(),
+			's+': date.getSeconds()
+		};
+		for (let k in o) {
+			if (new RegExp(`(${k})`).test(fmt)) {
+				const val = o[k] + '';
+				fmt = fmt.replace(RegExp.$1, val > 9 ? val : '0' + val);
+			}
+		}
+		return fmt;
+	},
 	CODE
 };
